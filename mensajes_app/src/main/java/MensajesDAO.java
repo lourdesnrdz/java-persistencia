89c92ab7d0fa350e99e37afbf175dd03d28bddff
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-05 13:46:39 -0500

    create messages

--- mensajes_app/src/main/java/MensajesDAO.java
+++ mensajes_app/src/main/java/MensajesDAO.java
@@ -1,6 +1,32 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.function.Predicate;
+
 public class MensajesDAO {
     public static void CrearMensajeDB(Mensajes mensaje){
+        Conexion cd_connect = new Conexion();
 
+        try (Connection conexion = cd_connect.get_connection()){
+            PreparedStatement ps = null;
+
+            try {
+                String query = "INSERT INTO `mensajes` (`mensaje`, `autor_mensaje`) VALUES (?, ?)";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutor_mensaje());
+                ps.executeUpdate();
+
+                System.out.println("mensaje creado");
+
+            }catch (SQLException e){
+                System.out.println(e);
+            }
+
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 
     public static void leerMensajesDB(){
